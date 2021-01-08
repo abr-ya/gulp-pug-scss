cc07b918f9e7f19ab6f60d8965a404cbfaa3b2cd
abr-ya <gamer05@list.ru>
2021-01-08 18:26:32 +0300

    Add scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
 gulp.task('pug-compile', () => {
@@ -14,9 +16,18 @@ gulp.task('pug-compile', () => {
     .pipe(browserSync.stream())
 });
 
+gulp.task('sass', () => {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass())
+    .pipe(concat('sass.css'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream())
+});
+
 gulp.task('default',() => {
   browserSync.init({
     server: { baseDir: './dist' }
   });
-  gulp.watch('src/**/*.pug', gulp.series('pug-compile'))
+  gulp.watch('src/**/*.pug', gulp.series('pug-compile'));
+  gulp.watch('src/**/*.scss', gulp.series('sass'));
 });
